4965203b509eda19dcc4fcf6fdd43bb6e50d027b
Alexandr <tolstov.alexandr.85@gmail.com>
2019-04-12 17:48:00 +0600

    1

---
+++ UPS/BattaryData.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ClosedXML.Excel;
+
+namespace UPS
+{
+    class BattaryData
+    {
+        readonly string name;
+        readonly string artLeg;
+        readonly string artOther;
+        readonly string nameOtherBrand;
+        readonly Dictionary<string, double> dischConst;
+
+        //Наименование АКБ
+        public string Name
+        {
+            get => name;
+        }
+        //Артикул АКБ Legrand
+        public string ArtLeg
+        {
+            get => artLeg;
+        }
+        //Артикул АКБ Другого бренда
+        public string ArtOther
+        {
+            get => artOther;
+        }
+        //Имя другого бренда
+        public string NameOtherBrand
+        {
+            get => nameOtherBrand;
+        }
+        //Разрядная характеристика
+        public Dictionary<string, double> DischConst
+        {
+            get => dischConst;
+        }
+
+        //Конструктор
+        public BattaryData(
+            string name, 
+            string artLeg,
+            string artOther,
+            string nameOtherBrand,
+            float const5m,
+            float const10m,
+            float const15m,
+            float const20m,
+            float const30m,
+            float const45m,
+            float const60m,
+            float const2h,
+            float const3h,
+            float const5h,
+            float const10h,
+            float const20h)
+        {
+            this.name = name;
+            this.artLeg = artLeg;
+            this.artOther = artOther;
+            this.nameOtherBrand = nameOtherBrand;
+            dischConst = new Dictionary<string, double>();
+            dischConst.Add("5 min", const5m);
+            dischConst.Add("10 min", const10m);
+            dischConst.Add("15 min", const15m);
+            dischConst.Add("20 min", const20m);
+            dischConst.Add("30 min", const30m);
+            dischConst.Add("45 min", const45m);
+            dischConst.Add("60 min", const60m);
+            dischConst.Add("2 h", const2h);
+            dischConst.Add("3 h", const3h);
+            dischConst.Add("5 h", const5h);
+            dischConst.Add("10 h", const10h);
+            dischConst.Add("20 h", const20h);
+        }
+
+        public void WriteToFile()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                string workDirectory = Environment.CurrentDirectory;//Читает путь с файлом exe
+                string folderData = "/data";//Имя папки
+
+                //В случае если папка data отсутствует создаем ее
+                DirectoryInfo dirInfo = new DirectoryInfo(workDirectory + folderData);
+                if (!dirInfo.Exists)
+                {
+                    dirInfo.Create();
+                }
+
+                //Запись файла
+                string fileName = Name + ".xlsx";//Имя файла
+                string fullPathName = workDirectory + folderData + "/" + fileName;//Полный путь с именем файла
+
+                var worksheet = workbook.Worksheets.Add("Discharge constant");
+
+                int i = 1;
+                foreach(var pair in DischConst)
+                {
+                    worksheet.Column(i).Cell(1).Value = pair.Key;
+                    worksheet.Column(i).Cell(2).Value = pair.Value;
+                    i++;
+                }
+                i = 1;
+                workbook.SaveAs(fullPathName);
+            }
+            
+        }
+    }
+}
--- UPS/MainWindow.xaml.cs
+++ UPS/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using ClosedXML.Excel;
 
 namespace UPS
 {
@@ -23,6 +25,48 @@ namespace UPS
         public MainWindow()
         {
             InitializeComponent();
+            using(var workbook = new XLWorkbook())
+            {
+                string workDirectory = Environment.CurrentDirectory;//Читает путь с файлом exe
+                string folderData = "/data";//Имя папки
+
+                //В случае если папка data отсутствует создаем ее
+                DirectoryInfo dirInfo = new DirectoryInfo(workDirectory + folderData);
+                if (!dirInfo.Exists)
+                {
+                    dirInfo.Create();
+                }
+
+                //Запись файла
+                string fileName = "HelloWorld.xlsx";//Имя файла
+                string fullPathName = workDirectory + folderData + "/" + fileName;//Полный путь с именем файла
+
+                var worksheet = workbook.Worksheets.Add("Sample Sheet");
+                worksheet.Cell("A1").Value = "Hello Worldssss!";
+                worksheet.Cell("A2").FormulaA1 = "=MID(A1, 7, 5)";
+                worksheet.Cell("A20").Value = "Эх ма";
+
+
+                workbook.SaveAs(fullPathName);
+
+                //Чтение файла
+                var readWorkbook = new XLWorkbook(fullPathName);
+                var readWorksheet = readWorkbook.Worksheet(1);
+                //var rows = worksheet.RangeUsed().RowsUsed();
+
+                //Вывод 
+
+                //Вариант 1
+                //Label1.Content = readWorksheet.Cell("A20").Value; 
+
+                //Вариант 2
+                //Label1.Content = readWorksheet.Column(1).Cell(20).Value;
+
+                BattaryData SK12 = new BattaryData("SK12-140", "311000", "HRL12540WFR", "Hitachi", 690, 512, 427, 343, 258, 195, 154, 90.6f, 69.5f, 45.9f, 25.7f, 14.04f);
+                Label1.Content = SK12.DischConst["10 min"];
+
+                SK12.WriteToFile();
+            }
         }
     }
 }
